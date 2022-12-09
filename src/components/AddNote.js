00db693957b84b958531af0d05cacdc521a25f6e
mayaemer <71705519+mayaemer@users.noreply.github.com>
2022-12-09 18:28:32 +0000

    created states for each input type for the note, using onChange event listener to set state of each

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -4,37 +4,61 @@ import { useState } from "react";
 
 function AddNote() {
 
-  const [colour, setColour] = useState('');
+  const colours = [
+    {value: '#b3f0ff', text: 'Blue'},
+    {value: '#ffcce6', text: 'Pink'},
+    {value: '#ffbb99', text: 'Peach'},
+    {value: '#b3e6b3', text: 'Green'},
+  ];
 
-  const createNote = () => {};
+  const initialVal = '' ;
 
-  const getData = (e) => {
-    console.log('test')
+  const [selectedColour, setSelectedColour] = useState(colours[0].value);
+  const [noteBody, setNoteBody] = useState('');
+  const [noteTitle, setNoteTitle] = useState('');
+
+  const [array, setArray] = useState([]);
+
+
+
+
+  const getTitle = (e) => {
+    setNoteTitle(e.target.value);
+  }
+
+  const getNote = (e) => {
+    setNoteBody(e.target.value);
   };
 
-  console.log(colour)
+  const getColour = (e) => {
+    setSelectedColour(e.target.value);
+  };
+
   
     return (
         <Form>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Enter your title:</Form.Label>
-          <Form.Control type="text" />
+          <Form.Control type="text" onChange={getTitle}/>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Enter your note:</Form.Label>
-          <Form.Control as="textarea" rows={3} />
+          <Form.Control as="textarea" rows={3} onChange={getNote}/>
         </Form.Group>
         <Form.Group className="mb-3">
           <Form.Label>Select a colour:</Form.Label>
-          <Form.Select onChange={getData}>
-            <option>Blue</option>
-            <option>Peach</option>
-            <option>Green</option>
-            <option>Pink</option>
+          <Form.Select value={selectedColour} onChange={getColour}>
+          {colours.map(colour => (
+          <option key={colour.value} value={colour.value}>
+            {colour.text}
+          </option>
+          ))}
           </Form.Select>
         </Form.Group>
         <button onClick={createNote}>Add</button>
         </Form>
+
+
     );
   }
   
